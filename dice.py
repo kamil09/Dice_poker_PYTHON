be37422318b7d6b7028457ca8778c21188e8c581
Kamil <kpiotrowski@comea.pl>
2016-11-10 23:54:17 +0100

    poker logic unit tests

--- dice.py
+++ dice.py
@@ -7,7 +7,6 @@ def rand(number):
     return dices
 
 def checkPoker(diceList):
-    if(len(diceList)!=5): return False, -1
     if(len(set(diceList))==1): return True, diceList[0]
     return False, 0
 
@@ -15,8 +14,7 @@ def checkKareta(diceList):
     return checkCountHelper(diceList, 4)
 
 def checkStrit(diceList):
-    if(len(diceList)!=5): return False, -1
-    if(len(set(diceList))==5):
+    if(len(set(diceList))==5 and (max(diceList)-min(diceList)==4)):
         if(1 in diceList): return True, 1
         else: return True, 2
     return False, 0
@@ -25,39 +23,59 @@ def checkThree(diceList):
     return checkCountHelper(diceList, 3)
 
 def check2pair(diceList):
-    if(len(diceList)!=5): return False, -1
     res, val = check1pair(diceList)
     if(res==False): return False, val
-    newDiceList = []
-    for i in diceList:
-        if (i != val): newDiceList.append(i)
-    res, val2 = check1pair(diceList)
+    res, val2 = check1pair(deleteFromList(diceList,val))
     if (res == True): return True, max([val2,val])*10 + min([val2,val])
     return False, 0
 
 def checkFull(diceList):
     res, val = checkThree(diceList)
     if(res==False): return False, val
-    newDiceList = []
-    for i in diceList:
-        if(i!=val): newDiceList.append(i)
-    res, val2 = check1pair(diceList)
-    if(res==True): return True, val2*10+val
+    res, val2 = check1pair( deleteFromList(diceList,val) )
+    if(res==True): return True, val*10+val2
     return False, 0
 
 def check1pair(diceList):
     return checkCountHelper(diceList, 2)
 
 def checkNothing(diceList):
-    if(len(diceList)!=5): return False, -1
     return True, max(diceList)
 
-pokerFunctions = [checkPoker, checkKareta, checkFull, checkStrit, checkThree, check2pair, check1pair, checkNothing]
+def checkWinner(diceList1, diceList2):
+    for fi, func in enumerate(pokerFunctions):
+        res1, val1 = func(diceList1)
+        res2, val2 = func(diceList2)
+        if(res1==False and res2==False): continue
+        if(res1==True and res2==False): return 1
+        if(res2==False and res2==True): return 2
+        #The same figures
+        if(val1>val2): return 1
+        if(val2>val1): return 2
+        #The same figures and values
+        diceList1 = deleteFigures[fi](diceList1,val1)
+        diceList2 = deleteFigures[fi](diceList2,val2)
+    return 0 #Remis
 
 def checkCountHelper(diceList, num):
-    if(len(diceList)!=5): return False, -1
-    sum = [0, 0, 0, 0, 0, 0]
-    for i, k in range(diceList): sum[i] += 1
-    for i in sum:
+    sum = [0, 0, 0, 0, 0, 0, 0]
+    for i in diceList: sum[i] += 1
+    for i, k in enumerate(sum):
         if (k == num): return True, i
     return False, 0
+
+def deleteFromList(diceList, num):
+    newDiceList = []
+    for i in diceList:
+        if (i != num): newDiceList.append(i)
+    return newDiceList
+
+def simpleFDel(diceList, num):
+    if(num<=6): return deleteFromList(diceList, num)
+    else: return deleteFromList(deleteFromList(diceList, (num-(num%10))/10) , (num%10) )
+
+def returnEmptyArray(diceList, num):
+    return []
+
+pokerFunctions  = [checkPoker, checkKareta, checkFull, checkStrit, checkThree, check2pair, check1pair, checkNothing]
+deleteFigures   = [returnEmptyArray, simpleFDel, returnEmptyArray, returnEmptyArray, simpleFDel, simpleFDel, simpleFDel, simpleFDel ]
--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import unittest
 import dice
 
 #UNIT TESTS :)
-class TestAiMethods(unittest.TestCase):
+class TestDicesMethods(unittest.TestCase):
 
     def test_rand(self):
         result = dice.rand(100)
@@ -20,10 +20,73 @@ class TestAiMethods(unittest.TestCase):
         self.assertFalse(result, "Should not be Poker")
         self.assertEqual(val, 0, "Poker should return 0 value")
 
-    def test_poker_false(self):
-        result, val = dice.checkPoker([1, 3, 3, 3])
-        self.assertFalse(result, "Should not be Poker")
-        self.assertEqual(val, -1, "Poker should return -1 value")
+    def test_kareta_true(self):
+        result, val = dice.checkKareta([1,2,2,2,2])
+        self.assertTrue(result, "Should be Kareta")
+        self.assertEqual(val, 2, "Kareta should return different value")
+
+    def test_kareta_false(self):
+        result, val = dice.checkKareta([1,2,4,4,4])
+        self.assertFalse(result, "Should not be Kareta")
+        self.assertEqual(val, 0, "Kareta should return different value")
+
+    def test_Full_true(self):
+        result, val = dice.checkFull([2, 2, 4, 4, 4])
+        self.assertTrue(result, "Should be Full")
+        self.assertEqual(val, 42, "Full should return different value")
+
+    def test_check_Strit_Small(self):
+        result, val = dice.checkStrit([1, 2, 3, 4, 5])
+        self.assertTrue(result, "Should be Strit")
+        self.assertEqual(val, 1, "Strit should return different value")
+
+    def test_check_Strit_Big(self):
+        result, val = dice.checkStrit([2, 3, 4, 5, 6])
+        self.assertTrue(result, "Should be Strit")
+        self.assertEqual(val, 2, "Strit should return different value")
+
+    def test_check_Strit_false(self):
+        result, val = dice.checkStrit([1, 3, 4, 5, 6])
+        self.assertFalse(result, "Should not be Strit")
+        self.assertEqual(val, 0, "Strit should return different value")
+
+    def test_check_Three_false(self):
+        result, val = dice.checkThree([1, 3, 2, 4, 4])
+        self.assertFalse(result, "Should not be Three")
+        self.assertEqual(val, 0, "Three should return different value")
+
+    def test_check_Three_true(self):
+        result, val = dice.checkThree([1, 3, 4, 4, 4])
+        self.assertTrue(result, "Should be Three")
+        self.assertEqual(val, 4, "Three should return different value")
+
+    def test_check_2pairs_true(self):
+        result, val = dice.check2pair([1, 3, 3, 4, 4])
+        self.assertTrue(result, "Should be 2 Pairs")
+        self.assertEqual(val, 43, "2 Pairs should return different value")
+
+    def test_check_2pairs_false(self):
+        result, val = dice.check2pair([1, 2, 3, 4, 4])
+        self.assertFalse(result, "Should not be 2 Pairs")
+        self.assertEqual(val, 0, "2 Pairs should return different value")
+
+    def test_check_nothing(self):
+        result, val = dice.checkNothing([1, 2, 3, 4, 6])
+        self.assertTrue(result, "Should be nothing")
+        self.assertEqual(val, 6, "2 Pairs should return different value")
+
+    def test_full_poker_game(self):
+        testData = [ # (firstList, secondList, whoShouldWin)
+            ([1, 2, 3, 4, 5], [2, 3, 4, 5, 6], 2),  #Lets add some test cases to be sure if it's correct
+            ([5, 5, 5, 5, 5], [4, 4, 4, 4, 1], 1),
+            ([4, 4, 4, 4, 1], [4, 4, 4, 4, 1], 0),
+            ([4, 4, 4, 4, 1], [4, 4, 4, 4, 2], 2),
+            ([3, 3, 3, 2, 2], [2, 2, 2, 6, 6], 1)
+        ]
+        for i in testData:
+            self.assertEqual(i[2],dice.checkWinner(i[0],i[1]), i)
+
+
 
 if __name__ == '__main__':
     unittest.main()
