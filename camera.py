e222b01112530eab2443c5b7bed8786ce3892d1d
lewinms <lewin.michal@gmail.com>
2016-11-16 17:48:08 +0100

    Próby rysowania

--- camera.py
+++ camera.py
@@ -1,8 +1,39 @@
 import cv2
 import numpy as np
+from skimage import color,io
+import matplotlib as mp
+from skimage.restoration import denoise_bilateral
+
+def passing(x):
+    pass
+
+def autoCanny(image,sigma=0.33):
+    med = np.median(image)
+    dolny = int(max(0,(1.0 - sigma)*med))
+    gorny = int(min(255, (1.0 + sigma) * med))
+    edges = cv2.Canny(image,dolny,gorny)
+    return edges
+
+# trackbars
+track = "Image"
+cv2.namedWindow(track)
+cv2.createTrackbar("c1", track, 130, 255, passing)
+# cv2.createTrackbar("c2", track, 10, 500, passing)
+cv2.createTrackbar("c3", track, 10, 500, passing)
+cv2.createTrackbar("c4", track, 10, 500, passing)
+cv2.createTrackbar("c5", track, 10, 100, passing)
+cv2.createTrackbar("c6", track, 1, 100, passing)
+cv2.createTrackbar("c7", track, 10, 100, passing)
+
+
+switch = '0 : OFF \n1 : ON'
+
 
 def findCircles(image):
     circles = cv2.HoughCircles(image,cv2.HOUGH_GRADIENT,1,40,param1=50,param2=30,minRadius=0,maxRadius=0)
+    # c6 = cv2.getTrackbarPos("c6",track)
+    # c7 = cv2.getTrackbarPos("c7",track)
+    # circles = cv2.HoughCircles(image,cv2.HOUGH_GRADIENT,c6,c7)
     image = cv2.cvtColor(image,cv2.COLOR_GRAY2BGR)
     if circles is None:
         return image
@@ -11,6 +42,20 @@ def findCircles(image):
         cv2.circle(image,(i[0],i[1]),i[2],(0,255,0),2) #Outter circle
         cv2.circle(image,(i[0],i[1]),2,(255,0,0),3) #Center of a circle
     return image
+
+def findSquares(image):
+    if image is None:
+        return image
+    image2, conturs, hierarchy = cv2.findContours(image.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # conturs = sorted(conturs,key = cv2.contourArea,reverse = True)[:20]
+    diceContours = []
+    for c in conturs:
+        perimeter = cv2.arcLength(c, True)
+        aprox = cv2.getTrackbarPos("c5",track)/100.0
+        approximation = cv2.approxPolyDP(c, aprox * perimeter, True)
+        # if len(approximation) == 4:
+        #     diceContours.appenddiceContours.append(approximation)
+    return diceContours
 
 def findDice(image):
     # fgbg = cv2.createBackgroundSubtractorMOG2()
@@ -23,18 +68,22 @@ def findDice(image):
     # cv2.imshow("test",grey)
     # clahe = cv2.createCLAHE(clipLimit=2.0,tileGridSize=(8,8))
     # grey = clahe.apply(grey)
-    grey = cv2.bilateralFilter(grey, 1, 20, 20)  # Blur image, remove noise but keep edges
+    # grey = cv2.bilateralFilter(grey, 1, 20, 20)  # Blur image, remove noise but keep edges
+    # edges = grey
     edges = cv2.Canny(grey, 10, 200)
-    image2, conturs, hierarchy = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+    image2, conturs, hierarchy = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # conturs = sorted(conturs,key = cv2.contourArea,reverse = True)[:20]
     diceContours = []
     for c in conturs:
         perimeter = cv2.arcLength(c, True)
+        #Nie wiem czy to jest ok.
+        perimeter *=5
         aprox = 0.02
         approximation = cv2.approxPolyDP(c, aprox * perimeter, True)
         if len(approximation) == 4:
             diceContours.append(approximation)
+        # diceContours.append(approximation)
 
     edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
     # circle = findCircles(edges)
@@ -42,8 +91,83 @@ def findDice(image):
         # print(d)
         # cv2.drawContours(circle, [d], -1, (0, 0, 255), 3)
         cv2.drawContours(edges, [d], -1, (0, 0, 255), 3)
-    # cv2.imshow("Circles",circle)
-    cv2.imshow("edges",edges)
+    # cv2.imshow(track,circle)
+    cv2.imshow(track,edges)
+
+
+
+def testowanie(image):
+    img = image
+    # cv2.imshow("test",img)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # for i in range(len(img)):
+    #     for j in range(len(img[i])):
+    #         img[i][j][0] = int((img[i][j][0]/10))*10
+    # img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    # min = cv2.getTrackbarPos("c1",track)
+    # max = cv2.getTrackbarPos("c2", track)
+    # min = 0
+    # max = 30
+    # img = cv2.inRange(img,np.array([min,0,0]),np.array([max,255,255]))
+    # img[:,:,0] =
+    kernelSharpen = np.array([[-1, -1, -1, -1, -1],
+                                 [-1, 2, 2, 2, -1],
+                                 [-1, 2, 8, 2, -1],
+                                 [-1, 2, 2, 2, -1],
+                                 [-1, -1, -1, -1, -1]]) / 8.0
+
+    # kernelSharpen = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+    # img = cv2.filter2D(img,-1,kernelSharpen)
+    # cv2.imshow("sharpen",img)
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    linie = cv2.HoughLines(img, 1, np.pi / 180, 200)
+    print(linie)
+
+    # img = cv2.equalizeHist(img)
+
+    # c2 = cv2.getTrackbarPos("c2", track)
+    c3 = cv2.getTrackbarPos("c3", track)
+    c4 = cv2.getTrackbarPos("c4", track)
+
+    img = cv2.bilateralFilter(img, 10, c3, c4)  # Blur image, remove noise but keep edges
+
+    c1 = cv2.getTrackbarPos("c1",track)
+    # ret, img = cv2.threshold(img, c1, 255, cv2.THRESH_BINARY_INV)
+    ret, img = cv2.threshold(img, c1, 255, cv2.THRESH_BINARY)
+    # th, img = cv2.threshold(img, c1, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    # ret, img = cv2.threshold(img, c1, 255, cv2.THRESH_TOZERO_INV)
+
+    # pixels = cv2.getTrackbarPos("convert",track)
+    kernel = np.ones((5, 5), np.uint8)
+    img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+    img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+    # img = cv2.equalizeHist(img)
+
+    img = autoCanny(img)
+
+    # img = cv2.Laplacian(img, cv2.CV_64F)
+    # img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+
+    # img = cv2.Sobel(img,10,c2,c3)
+    # c3 = cv2.getTrackbarPos("c3", track)
+    #
+    # img = cv2.bilateralFilter(img, c1, c2, c3)  # Blur image, remove noise but keep edges
+
+    # img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+    # cv2.imshow("test",cv2.cvtColor(image,cv2.COLOR_BGR2GRAY))
+
+    dices = findSquares(img)
+    # dices = None
+    # img = findCircles(img)
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    if (dices is not None):
+        for d in dices:
+            # print(d)
+            # cv2.drawContours(circle, [d], -1, (0, 0, 255), 3)
+            cv2.drawContours(img, [d], -1, (0, 0, 255), 3)
+
+    cv2.imshow(track,img)
 
 
 def playCamera(camera):
@@ -51,7 +175,9 @@ def playCamera(camera):
     while (True):
         check,klatka = cap.read()
         if check == True:
-            findDice(klatka)
+
+            # findDice(klatka)
+            testowanie(klatka)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
@@ -63,5 +189,6 @@ if __name__ == '__main__':
     # image = cv2.imread("dice.jpg")
     # cv2.imshow("obraz",image)
     # findDice(image)
+    # testowanie(image)
     # cv2.waitKey(0)
     playCamera(0)
